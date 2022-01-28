0466ffd052f1ba3d85441356e4c5ca27791e92cf
Kingstalux <kingsleyngu25@gmail.com>
2022-01-28 06:22:09 +0100

    Add rspec

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,19 @@
+require_relative './spec_helper'
+
+# describe Book do
+#   before :each do
+#     @book = Book.new('Inferno', 'Dan Brown')
+#   end
+
+#   context 'create a book instance' do
+#     it 'takes two parameters and returns a Book object' do
+#       expect(@book).to be_an_instance_of Book
+#     end
+
+#     describe '#author' do
+#       it 'returns the correct author' do
+#         expect(@book.author).to eql 'Dan Brown'
+#       end
+#     end
+#   end
+# end
